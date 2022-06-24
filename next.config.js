fc87fa78c00a5076932cfc52283adac9cc8f226e
Daniel Lins <dlins300@gmail.com>
2022-06-24 20:10:54 -0300

    corrigido um pequeno bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["cdn.traction.one"],
   },
 };
+
+module.exports = nextConfig;
